ed0a1bb6af0c262409528218fe700961a0a6e199
emilie <loveskyship@gmail.com>
2023-07-05 21:08:18 -0700

    update mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(logger);
 
@@ -35,4 +40,17 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on prot ${PORT}`));
+// wrap app.listen in the mongoose connection
+mongoose.connection.once("open", () => {
+  console.log("connected");
+  app.listen(PORT, () => console.log(`Server running on prot ${PORT}`));
+});
+
+// tracks the err and writes in the mongoErrLog.log for testing
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
